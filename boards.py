f9c3fa0dcd97e29f07012b930c01b567fba6270b
madisonmacneil <maccmadison@gmail.com>
2023-12-07 18:30:19 -0500

    boards.py file

---
+++ boards.py
@@ -0,0 +1,47 @@
+BOARDS = [[
+    ["White", "Green", "Yellow", "White", "White"],
+    ["Yellow", "Green", "White", "White", "White"],
+    ["White", "Green", "White", "Yellow", "White"],
+    ["Green", "Green", "Green", "Green", "Green"],
+], [
+    ["White", "Yellow", "White", "White", "White"],
+    ["White", "White", "Green", "White", "White"],
+    ["White", "Yellow", "Green", "Green", "White"],
+    ["Green", "Green", "Green", "Green", "Green"],
+],[
+    ["White", "White", "White", "Yellow", "White"],
+    ["White", "White", "White", "Green", "Green"],
+    ["White", "White", "Green", "Green", "Green"],
+    ["Green", "Green", "Green", "Green", "Green"],
+],[
+    ["White", "White", "Yellow", "White", "White"],
+    ["Yellow", "Yellow", "Yellow", "White", "White"],
+    ["White", "Yellow", "Yellow", "White", "White"],
+    ["Green", "Green", "Green", "Green", "Green"],
+],[
+    ["White", "Yellow", "Yellow", "White", "Green"],
+    ["Green", "White", "White", "Green", "Green"],
+    ["Green", "White", "Green", "Green", "Green"],
+    ["Green", "Green", "Green", "Green", "Green"],
+],[
+    ["White", "White", "White", "Yellow", "White"],
+    ["White", "White", "Yellow", "White", "White"],
+    ["Green", "White", "Yellow", "White", "White"],
+    ["Green", "Green", "Green", "Green", "Green"],
+],[
+    ["White", "Yellow", "White", "White", "White"],
+    ["White", "Yellow", "White", "Yellow", "Yellow"],
+    ["White", "White", "Yellow", "Green", "Green"],
+    ["Green", "Green", "Green", "Green", "Green"],
+],[
+    ["Green", "White", "White", "White", "White"],
+    ["Green", "Yellow", "Yellow", "White", "White"],
+    ["Green", "Yellow", "White", "Yellow", "White"],
+    ["Green", "Green", "Green", "Green", "Green"],
+], [
+    ["White", "White", "Yellow", "White", "Yellow"],
+    ["Yellow", "White", "Green", "White", "White"],
+    ["White", "White", "Green", "Green", "Green"],
+    ["Green", "Green", "Green", "Green", "Green"],
+], 
+]
